356eb7527d7f21c8095c6a294e25af189c0d4afc
oyku <oyku@DESKTOP-73QGJ2T>
2022-08-10 23:26:55 +0300

    lesson02

--- app.js
+++ app.js
@@ -2,11 +2,27 @@ const express = require('express');
 
 const app = express();
 
+//Template Engine
+app.set("view engine","ejs");
+
+//Middlewares
+app.use(express.static("public"));//--> statik dosyaların nerede olduğunu belirttik
+
+//Routes
+app.get('/', (req, res) => {
+  res.status(200).render('index',{
+    page_name: "index"
+  });
+});
+app.get('/about', (req, res) => {
+    res.status(200).render('about',{
+      page_name:"about"
+    });
+  });
+
+
 const port = 5833;
 
-app.get('/', (req, res) => {
-  res.send('Index Sayfası');
-});
 
 app.listen(port, () => {
   console.log(`App started on port ${port}`);
